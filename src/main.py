0988001fe49961a391d4d5359ba1d16eb2793b28
AlexanderGuacan <alexander.guacan2003@gmail.com>
2022-12-08 04:24:26 -0500

    Actualizando documentacion y inicializando juego desde un menu grafico

--- src/main.py
+++ src/main.py
@@ -2,11 +2,10 @@
 Tema: Palabras encadenadas
 
 Descripcion:
-El juego de las palabras encadenadas, es un juego clasico, el cual desafia la rapidez y memoria del jugador.
+El juego de las palabras encadenadas, es un juego clasico, el cual desafia la memoria del jugador y su conocimiento de las palabras.
 Consiste en que cada jugador dira una palabra de cualquier ambito y el siguiente jugador debera decir una nueva palabra
 que comienze por la letra de la letra final de la ultima palabra mencionada y que ademas no se haya mencionada previamente.
-Cada jugador se eliminara si falla en alguna de las reglas mencionadas y ya no podra participar, el ganador sera aquel jugador
-que quede al final del juego.
+Para este juego unicamente sera solo un jugador el que debera ingresar las palabras, mostrando en un inicio una palabra al azar.
 
 Autor: Alexander David Guacan Rivera
 
@@ -14,47 +13,36 @@ Version: 1.4
 """
 
 # Importamos la clase Game
-from Game import *
+from MenuGraphic import MenuGraphic
+# Importamos la clase MenuGame
+from MenuGame import MenuGame
 
 # Definimos la funcion principal desde donde empezara el programa
 def main() -> None:
-    """_summary_ La funcion sirve como punto inicial del programa
-
-        Returns:
-        - None
+    """_summary_ Esta es la funcion principal desde donde empezara a ejecutarse el juego. Haciendo uso de objetos de las clases MenuGraphic y Menu Game. Se presentara al usuario las opciones del menu y solo terminara el juego cuando el jugador seleccione la opcion de "Salir"
     """
 
-    # Limpiamos la terminal
-    os.system("cls")
-
-    # Imprimimos las reglas del juego
-    print("""\t\t\tPALABRAS ENCADENADAS
-
-COMO JUGAR: EL JUEGO INICIARA CON UNA PALABRA AL AZAR, SU OBJETIVO DEBERA SER INGRESAR PALABRAS
-QUE EMPIEZEN POR LA ULTIMA LETRA DE LA PALABRA PRESENTADA
-REGLAS:
-- NO SE ACEPTAN PALABRAS QUE INICIEN POR VOCALES CON TILDE
-- TIENE 3 VIDAS, CADA PALABRA MAL INGRESADA O REPETIDA LE RESTARA UNA VIDA
-- DEBE INGRESAR HASTA 5 PALABRAS DE FORMA CORRECTA PARA GANAR
-- INGRESE PALABRAS DE MAS DE 4 LETRAS
+    # Creamos una instancia de la clase MenuGraphic. Inicializando el constructor con el titulo y opciones del menu principal
+    menu_gui = MenuGraphic(
+    "\t\tPALABRAS ENCADENADAS",
+        [
+            "JUGAR",
+            "INSTRUCCIONES",
+            "SALIR"
+        ]
+    )
     
-[PRESIONE CUALQUIER TECLA PARA COMENZAR EL JUEGO]""")
+    # Creamos variable booleana que nos determinara cuando se debera terminar la ejecucion del juego
+    end_game = False
+    # Creamos una instancia de la clase MenuGame que se encargara de gestionar las opciones elegidas por el jugador
+    menu_game = MenuGame()
 
-    os.system("pause > nul")
+    # El juego se seguira ejecutando mientras la opcion seleccionada por el jugador no sea la de "Salir" en el menu
+    while not end_game:
+        # Asignamos el resultado de la opcion elegida en el menu, si fue la de "Salir" es verdadero, caso contrario cualquiera de las otras opciones es falsa
+        end_game = menu_game.select_option(menu_gui.print())
 
-
-    # Creamos una instancia de la clase Game
-    game = Game()
-
-    # Llamamos a la funcion miembro start de la clase Game. Guardamos en la variable is_winner el estado final del jugador si el mismo gano o no
-    is_winner = game.start()
-    
-    # Si el jugador gano el juego
-    if is_winner:
-        print("\nGanaste")
-    # Si el jugador no gano el juego
-    else:
-        print("\nPerdiste")
-
-# Llamamos a la funcion main para comenzar el programa
-main()
+# Verificamos que el programa esta siendo ejecutado desde el presente modulo
+if __name__ == "__main__":
+    # Llamamos a la funcion main para comenzar el programa
+    main()
